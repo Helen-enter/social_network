affeb5124c669e3b81443b737420ded739a3e989
Elena <esafonova961205@gmail.com>
2021-09-14 15:14:56 +0300

    init commit

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,63 +1,43 @@
 import React from "react";
 import styles from "./Users.module.css"
 import { UsersPropsType } from "./UsersContainer";
+import  axios from 'axios'
+import userPhoto from '../../../src/assets/images/user.png'
 
-let Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://www.m24.ru/b/d/nBkSUhL2gVMkn8-0PqzJrMCqzJ3w-pui1inGh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=YjarIArX-kmJrnA2Iq2RGg.jpg',
-                    followed: false,
-                    fullName: 'Svetlana',
-                    status: 'I do it!',
-                    location: {country: 'Belarus', city: 'Minsk'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://www.m24.ru/b/d/nBkSUhL2gVMkn8-0PqzJrMCqzJ3w-pui1inGh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=YjarIArX-kmJrnA2Iq2RGg.jpg',
-                    followed: true,
-                    fullName: 'Ekaterina',
-                    status: 'I do it!',
-                    location: {country: 'Russia', city: 'Moscow'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://www.m24.ru/b/d/nBkSUhL2gVMkn8-0PqzJrMCqzJ3w-pui1inGh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=YjarIArX-kmJrnA2Iq2RGg.jpg',
-                    followed: false,
-                    fullName: 'Egor',
-                    status: 'I do it!',
-                    location: {country: 'Ukraine', city: 'Kiev'}
-                },
-            ]
-        )
+class Users extends React.Component<any, any> {
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        })
     }
 
-
-    return <div>
+    render() {
+        return <div>
             {
-                props.users.map(u => <div key={u.id}>
+                this.props.users.map((u: { id: string | number | undefined; photos: { small: string | null | undefined; }; followed: any; name: React.ReactNode; status: React.ReactNode; }) => <div key={u.id}>
                         <span>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                         </span>
-                        <span>
+                    <span>
                             {u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
                         </span>
-                        <span>
+                    <span>
                             <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                                 <div>{u.status}</div>
                             </span>
                             <span>
-                                <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                                <div>{/*{u.location.country}*/}</div>
+                            <div>{/*{u.location.city}*/}</div>
                         </span>
                         </span>
-                    </div>)
+                </div>)
             }
         </div>
+    }
 }
 
 export default Users;
